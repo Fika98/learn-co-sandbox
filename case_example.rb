f2686d265e0bb6b435ac552937ce3b9b0692e4a9
Firuz Afzalov <afzalov98@gmail.com>
2021-06-30 16:21:31 +0000

    Automatically backed up by Learn

---
+++ case_example.rb
@@ -0,0 +1,25 @@
+# greeting = "friendly_greeting"
+
+# case greeting
+# when "unfriendly+greeting "
+#   puts " what you want!?"
+# when "friendly_greeting"
+#   puts "Hi! how are you?"
+
+# end
+
+current_weather = "raining"
+ 
+case current_weather
+  when "sunny"
+    puts "grab some sunscreen!"
+  when "raining"
+    puts "grab an umbrella"
+  when "snowing"
+    puts "bundle up"
+end
+
+
+
+
+
